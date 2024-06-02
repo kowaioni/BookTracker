6a7f51fd0f86f91d6b761a6e08f2c99c7cbffc22
kowaioni <163346055+kowaioni@users.noreply.github.com>
2024-06-02 04:01:57 +0300

    change variable

--- routes.rs
+++ routes.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate rocket;
 
+use rocket::serde::{json::Json, Deserialize, Serialize};
+
 #[derive(FromForm, Serialize, Deserialize)]
 struct Book {
     id: Option<i32>,
@@ -8,28 +10,28 @@ struct Book {
     year: i32,
 }
 
-#[post("/books", data = "<book>")]
-fn create_book(book: Json<Book>) -> Json<Book> {
-   book
+#[post("/books", data = "<new_book>")]
+fn add_new_book(new_book: Json<Book>) -> Json<Book> {
+   new_book
 }
 
-#[get("/books/<id>")]
-fn get_book(id: i32) -> Option<Json<Book>> {
+#[get("/books/<book_id>")]
+fn fetch_book_by_id(book_id: i32) -> Option<Json<Book>> {
     None
 }
 
-#[put("/books/<id>", data = "<book>")]
-fn update_book(id: i32, book: Json<Book>) -> Option<Json<Book>> {
-    Some(book)
+#[put("/books/<book_id>", data = "<updated_book>")]
+fn modify_existing_book(book_id: i32, updated_book: Json<Book>) -> Option<Json<Book>> {
+    Some(updated_book)
 }
 
-#[delete("/books/<Now that everything but the code has been stripped away, let's get the results.<id>")]
-fn delete_book(id: i32) -> Option<Json<()>> {
+#[delete("/books/<book_id>")]
+fn remove_book(book_id: i32) -> Option<Json<()>> {
     Some(Json(()))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![create_book, get_book, update_book, delete_book])
+        .mount("/", routes![add_new_book, fetch_book_by_id, modify_existing_book, remove_book])
 }
