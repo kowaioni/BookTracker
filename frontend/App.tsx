6ecad67a31bd5890e9b521e11b78d7fece873018
kowaioni <163346055+kowaioni@users.noreply.github.com>
2024-06-13 23:02:42 +0300

    implemented new feature

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BookForm } from './BookStateForm';
+import { BookForm } from './BookForm'; 
 import { BookList } from './BookList';
 import { NavBar } from './NavBar';
 import './MainComponent.css';
@@ -13,16 +13,32 @@ interface Book {
 
 interface MainComponentState {
   books: Book[];
+  editingBook?: Book; 
 }
 
-export class MainDeferredComponent extends Component<{}, MainComponentState> {
+export class MainComponent extends Component<{}, MainComponentState> { 
   state = {
     books: [],
+    editingBook: undefined, 
   };
 
   addBook = (book: Book): void => {
     this.setState(prevState => ({
       books: [...prevState.books, { ...book, id: Date.now() }],
+    }));
+  };
+
+  editBookStart = (id: number): void => {
+    const bookToEdit = this.state.books.find(book => book.id === id);
+    if (bookToEdit) {
+      this.setState({ editingBook: { ...bookToEdit } });
+    }
+  };
+
+  editBookSave = (editedBook: Book): void => {
+    this.setState(prevState => ({
+      books: prevState.books.map(book => book.id === editedBook.id ? editedBook : book),
+      editingBook: undefined, 
     }));
   };
 
@@ -46,14 +62,18 @@ export class MainDeferredComponent extends Component<{}, MainComponentState> {
   }
 
   render() {
-    const { books } = this.state;
+    const { books, editingBook } = this.state;
 
     return (
       <div className="mainComponent">
         <NavBar />
         <div className="content">
-          <BookForm addBook={this.addAuthBook} />
-          <BookList books={books} deleteBook={this.deleteBook} />
+          <BookForm addBook={this.addBook} editingBook={editingBook} saveEdit={this.editBookSave} />
+          <BookList 
+            books={books} 
+            deleteBook={this.deleteBook} 
+            startEdit={this.editBookStart} 
+          />
         </div>
       </div>
     );
